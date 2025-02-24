8f2bc3174d4444fcff2f2d1221b447f1b6965152
GiGomieri05 <giovannidelucagomieri@hotmail.com>
2025-02-24 09:43:18 -0300

    Add IconButton component and update Button component to accept props

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,18 @@
-import { ArrowRight } from "lucide-react";
+import { ArrowRight, Copy } from "lucide-react";
 import { Button } from "@/components/button";
+import { IconButton } from "@/components/icon-button";
 
 export default function Home() {
   return (
     <main>
-      <Button>
+      <Button type="submit">
         Enviar
         <ArrowRight />
       </Button>
+
+      <IconButton>
+        <Copy />
+      </IconButton>
     </main>
   );
 }
