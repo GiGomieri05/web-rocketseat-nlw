3cf6d9126074dfb08bf644127129f991e92adcb9
GiGomieri05 <giovannidelucagomieri@hotmail.com>
2025-02-24 09:30:17 -0300

    Add lucide-react dependency, update button component, and enhance global styles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,13 @@
+import { ArrowRight } from "lucide-react";
 import { Button } from "@/components/button";
 
 export default function Home() {
   return (
     <main>
-      <div className="text-white">Hello world!</div>
-
-      <h1 className="text-4xl font-semibold font-heading">Hello, world!</h1>
-
-      <Button text="Gico" />
-      <Button text="RDB" />
-      <Button text="oi!" />
+      <Button>
+        Enviar
+        <ArrowRight />
+      </Button>
     </main>
   );
 }
