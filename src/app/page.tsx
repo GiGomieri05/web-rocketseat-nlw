bd713115aa7caeda5ba7af7fb172f740f35cee92
GiGomieri05 <giovannidelucagomieri@hotmail.com>
2025-02-24 09:19:28 -0300

    Enhance layout and styling with Tailwind CSS, add custom theme colors, and implement button component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,15 @@
+import { Button } from "@/components/button";
+
 export default function Home() {
   return (
     <main>
-      <div>Hello world!</div>
+      <div className="text-white">Hello world!</div>
+
+      <h1 className="text-4xl font-semibold font-heading">Hello, world!</h1>
+
+      <Button text="Gico" />
+      <Button text="RDB" />
+      <Button text="oi!" />
     </main>
   );
 }
